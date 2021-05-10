d567082aaa98325144fb8a610bfc77ff6c45114f
gee bartlett <geeus81@gmail.com>
2021-05-10 19:54:31 +0100

    messing with graphics

--- A2600-dev/GameOfSkulls/GOS.asm
+++ A2600-dev/GameOfSkulls/GOS.asm
@@ -3,12 +3,122 @@
     include "vcs.h"
     include "macro.h"
 
+
+NTSC_LINES = #192
+PAL_LINES = #242
+
+
     ORG $F000
     CLEAN_START
-    VERTICAL_SYNC
-    
     
 
+StartOfFrame
 
+
+
+; Start of new frame
+; Start of vertical blank processing
+
+        lda #0
+        sta VBLANK
+        lda #2
+        sta VSYNC
+        sta WSYNC
+        sta WSYNC
+        sta WSYNC               ; 3 scanlines of VSYNC signal
+        lda #0
+        sta VSYNC           
+
+
+;------------------------------------------------
+
+; 37 scanlines of vertical blank...
+
+    
+
+        ldx #0
+
+VerticalBlank   sta WSYNC
+
+        inx
+
+        cpx #37
+
+        bne VerticalBlank
+
+       ;------------------------------------------------
+
+       ; Do 192 scanlines of color-changing (our picture)
+
+                ldx #45
+                stx COLUBK
+                ldx #0                ; this counts our scanline number
+                sta 60
+                
+                nop
+                
+
+Picture                      ; change background color (rainbow effect)
+                ;lda #1
+                
+                stx 60
+                sta RESP0
+                sta RESP1
+                ldx #$AA
+                stx GRP0
+                stx GRP1
+                sta WSYNC              ; wait till end of scanline
+
+
+                ldx 60
+                inx
+                
+                cpx PAL_LINES
+
+                bne Picture
+
+;--------------------------------------------       ;------------------------------------------------
+
+
+
+   
+
+ 
+
+                lda #%01000010
+
+                sta VBLANK          ; end of screen - enter blanking
+
+
+
+   ; 30 scanlines of overscan...
+
+
+
+                ldx #0
+
+Overscan        sta WSYNC
+
+                inx
+
+                cpx #30
+
+                bne Overscan
+
+
+
+
+
+
+
+
+
+----
+
+        jmp StartOfFrame ; return to begining of the frame
+
+
+    
+    seg
     ORG $FFFE
     .word $F000
